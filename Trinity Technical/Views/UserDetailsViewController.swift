8ae52356a3589b017af5a13c8509883acfddccc3
Yogi Priyo Prayogo <yogipriyo.prayogo@gmail.com>
2023-04-15 22:22:21 +0700

    removing main storyboard from cde and perform first good run

---
+++ Trinity Technical/Views/UserDetailsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  UserDetailsViewController.swift
+//  Trinity Technical
+//
+//  Created by Yogi Priyo Prayogo on 15/04/23.
+//
+
+import UIKit
+
+class UserDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
